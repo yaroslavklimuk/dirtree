0d324f3f433219052bd4f237cb36011a43b19285
Yaroslav Klimuk <y.klimuk@devcaz.com>
2021-10-12 22:29:52 +0200

    fix

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
-    "bytes"
-    "sort"
-    "strconv"
+	"sort"
+	"strconv"
 )
 
 type FileInfoByName []os.FileInfo
@@ -14,7 +14,13 @@ type FileInfoByName []os.FileInfo
 func (fin FileInfoByName) Len() int      { return len(fin) }
 func (fin FileInfoByName) Swap(i, j int) { fin[i], fin[j] = fin[j], fin[i] }
 func (fin FileInfoByName) Less(i, j int) bool {
-    return fin[i].Name() < fin[j].Name();
+	return fin[i].Name() < fin[j].Name()
+}
+
+type DirNode struct {
+	prev *DirNode
+	info os.FileInfo
+	last bool
 }
 
 const (
@@ -25,87 +31,100 @@ const (
 )
 
 func dismissFiles(dirContents []os.FileInfo) (res []os.FileInfo) {
-    for _, item := range dirContents {
-        if item.IsDir() == true {
-            res = append(res, item)
-        }
-    }
-    return
+	for _, item := range dirContents {
+		if item.IsDir() == true {
+			res = append(res, item)
+		}
+	}
+	return
 }
 
-func drawItem(buffer *bytes.Buffer, fInfo os.FileInfo, level int, emptyLevels int, last bool) {
-    notEmpty := level - emptyLevels
-    for i := 0; i < notEmpty - 1; i++ {
-        buffer.WriteString(Br_vert)
-        buffer.WriteString("\t")
-    }
-    for i := 0; i < emptyLevels; i++ {
-        buffer.WriteString("\t")
-    }
-    if last == true {
-        buffer.WriteString(Br_last  + Br_horiz + Br_horiz + Br_horiz + fInfo.Name())
-    } else {
-        buffer.WriteString(Br_split + Br_horiz + Br_horiz + Br_horiz + fInfo.Name())
-    }
-    if fInfo.IsDir() == false {
-        itemSize := fInfo.Size()
-        strItemSize := "empty"
-        if itemSize > 0 {
-            strItemSize = strconv.FormatInt(itemSize, 10) + "b"
-        }
-        buffer.WriteString(" (" + strItemSize + ")")
-    }
-    buffer.WriteString("\n")
+func drawItem(buffer *bytes.Buffer, node *DirNode) {
+	prevNode := node.prev
+	var itemLine string
+	for prevNode.prev != nil {
+		if prevNode.last == true {
+			itemLine = "\t" + itemLine
+		} else {
+			itemLine = Br_vert + "\t" + itemLine
+		}
+		prevNode = prevNode.prev
+	}
+	buffer.WriteString(itemLine)
+	if node.last == true {
+		buffer.WriteString(Br_last + Br_horiz + Br_horiz + Br_horiz + node.info.Name())
+	} else {
+		buffer.WriteString(Br_split + Br_horiz + Br_horiz + Br_horiz + node.info.Name())
+	}
+	if node.info.IsDir() == false {
+		itemSize := node.info.Size()
+		strItemSize := "empty"
+		if itemSize > 0 {
+			strItemSize = strconv.FormatInt(itemSize, 10) + "b"
+		}
+		buffer.WriteString(" (" + strItemSize + ")")
+	}
+	buffer.WriteString("\n")
 }
 
-func drawDirContents(buffer *bytes.Buffer, path string, printFiles bool, level int, emptyLevels int, last bool) (err error) {
-
-    currItem, err := os.Open(path)
-    if err != nil {
-	    return err
+func drawDirContents(buffer *bytes.Buffer, node *DirNode, path string, printFiles bool) (err error) {
+	currItem, err := os.Open(path)
+	if err != nil {
+		return err
 	}
 
-    dirContents, err := currItem.Readdir(-1)
-    if err != nil {
-	    return err
+	dirContents, err := currItem.Readdir(-1)
+	if err != nil {
+		return err
 	}
-    if printFiles == false {
-        dirContents = dismissFiles(dirContents)
-    }
+	if printFiles == false {
+		dirContents = dismissFiles(dirContents)
+	}
 
-    dirItemsCount := len(dirContents)
+	dirItemsCount := len(dirContents)
 
-    if dirItemsCount > 0 {
-        sort.Sort(FileInfoByName(dirContents))
-        if last == true {
-            emptyLevels++;
-        }
-        for i := 0; i < dirItemsCount; i++ {
-            itemPath := path + string(os.PathSeparator) + dirContents[i].Name()
-            itemInfo, err := os.Stat(itemPath)
-            if err != nil {
-		        return err
-	        }
-            isLast := i == dirItemsCount - 1
-            drawItem(buffer, itemInfo, level + 1, emptyLevels, isLast)
+	if dirItemsCount > 0 {
+		sort.Sort(FileInfoByName(dirContents))
+		for i := 0; i < dirItemsCount; i++ {
+			itemPath := path + string(os.PathSeparator) + dirContents[i].Name()
+			itemInfo, err := os.Stat(itemPath)
+			if err != nil {
+				return err
+			}
+			newNode := &DirNode{
+				prev: node,
+				info: itemInfo,
+				last: i == dirItemsCount-1,
+			}
+			drawItem(buffer, newNode)
 
-            if itemInfo.IsDir() == true {
-                rerr := drawDirContents(buffer, itemPath, printFiles, level + 1, emptyLevels, isLast)
-                if rerr != nil {
-                    return rerr;
-                }
-            }
-        }
-    }
-    return nil;
+			if itemInfo.IsDir() == true {
+				err = drawDirContents(buffer, newNode, itemPath, printFiles)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	err = currItem.Close()
+	return err
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) (err error) {
-    var buffer bytes.Buffer
-	err = drawDirContents(&buffer, path, printFiles, 0, 0, false)
-    bufferContents := buffer.String()
-    fmt.Fprintln(out, bufferContents[:len(bufferContents)-1])
-    return err
+	var buffer bytes.Buffer
+	itemInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	newNode := &DirNode{
+		info: itemInfo,
+		last: false,
+	}
+	err = drawDirContents(&buffer, newNode, path, printFiles)
+	bufferContents := buffer.String()
+	_, err = fmt.Fprint(out, bufferContents)
+	return err
 }
 
 func main() {
